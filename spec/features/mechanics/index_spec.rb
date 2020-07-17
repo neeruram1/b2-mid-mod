fe98750061c6bb4c48b233a4aabf1eb982f99eba
“Neeru <neeram85@gmail.com>
2020-07-17 09:43:22 -0600

    User story 1 complete

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Mechanics index page" do
     @mechanic1 = Mechanic.create(name: "Michael Bluth", years_of_exp: 10)
     @mechanic2 = Mechanic.create(name: "Tobias Funke", years_of_exp: 8)
   end
+
   it "I see a header saying All Mechanics and I see a list of their names and years of experience" do
 
     visit '/mechanics'
@@ -13,32 +14,11 @@ RSpec.describe "Mechanics index page" do
     end
 
     within(".mechanics-#{@mechanic1.id}") do
-      expect(page).to have_content("Name: #{@mechanic1.name}")
-      expect(page).to have_content("Years of Experience: #{@mechanic1.years_of_exp}")
+      expect(page).to have_content("#{@mechanic1.name} - #{@mechanic1.years_of_exp} years of experience")
     end
 
     within(".mechanics-#{@mechanic2.id}") do
-      expect(page).to have_content("Name: #{@mechanic2.name}")
-      expect(page).to have_content("Years of Experience: #{@mechanic2.years_of_exp}")
+      expect(page).to have_content("#{@mechanic2.name} - #{@mechanic2.years_of_exp} years of experience")
     end
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-And I see a list of all mechanic’s names and their years of experience
-Ex:
-             All Mechanics
-   Sam Mills - 10 years of experience
-   Kara Smith - 11 years of experience
