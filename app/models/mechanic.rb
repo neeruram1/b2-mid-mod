dd386f5967bfd3140c77142bc77ab932000499b7
“Neeru <neeram85@gmail.com>
2020-07-17 09:22:41 -0600

    Create MechanicRide model

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,5 @@
 class Mechanic < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :years_of_exp
+  has_many :mechanic_rides
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Mechanic, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :years_of_exp }
   end
+
+  describe "relationships" do
+    it { should have_many :mechanic_rides}
+  end
 end
