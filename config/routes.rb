1dc07dcd5ac479d14920776b8b0b2bf90d5ff525
“Neeru <neeram85@gmail.com>
2020-07-17 11:07:02 -0600

    Mechanic show page I see their info and all their rides complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
   get '/amusement_parks/:id', to: 'amusement_parks#show'
+  get '/mechanics/:id', to: 'mechanics#show'
 end
